afb1ed42466fb040f8fa32913424e85a72b810b1
Gerard Causse <grod.causse@gmail.com>
2025-02-15 23:18:30 -0500

    Using routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
+
+# from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+
+# from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
-from snippets.views import api_root, SnippetViewSet, UserViewSet
+
+# from snippets.views import api_root, SnippetViewSet, UserViewSet
 
 # urlpatterns = [
 #     path("snippets/", views.SnippetList.as_view()),
@@ -35,21 +39,27 @@ from snippets.views import api_root, SnippetViewSet, UserViewSet
 #     ]
 # )
 
-snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
-snippet_detail = SnippetViewSet.as_view(
-    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
-)
-snippet_highlight = SnippetViewSet.as_view(
-    {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
-)
-user_list = UserViewSet.as_view({"get": "list"})
-user_detail = UserViewSet.as_view({"get": "retrieve"})
+# snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
+# snippet_detail = SnippetViewSet.as_view(
+#     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+# )
+# snippet_highlight = SnippetViewSet.as_view(
+#     {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
+# )
+# user_list = UserViewSet.as_view({"get": "list"})
+# user_detail = UserViewSet.as_view({"get": "retrieve"})
 
-urlpatterns = [
-    path("", api_root),
-    path("snippets/", snippet_list, name="snippet-list"),
-    path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
-    path("snippets/<int:pk>/highlight/", snippet_highlight, name="snippet-highlight"),
-    path("users/", user_list, name="user-list"),
-    path("users/<int:pk>/", user_detail, name="user-detail"),
-]
+# urlpatterns = [
+#     path("", api_root),
+#     path("snippets/", snippet_list, name="snippet-list"),
+#     path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
+#     path("snippets/<int:pk>/highlight/", snippet_highlight, name="snippet-highlight"),
+#     path("users/", user_list, name="user-list"),
+#     path("users/<int:pk>/", user_detail, name="user-detail"),
+# ]
+
+router = DefaultRouter()
+router.register(r"snippets", views.SnippetViewSet, basename="snippet")
+router.register(r"users", views.UserViewSet, basename="user")
+
+urlpatterns = [path("", include(router.urls))]
