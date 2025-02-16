37636fea815bd53fd154a627857e859b3c803563
Gerard Causse <grod.causse@gmail.com>
2025-02-15 22:17:50 -0500

    Creating an endpoint for the highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # from django.http import HttpResponse, JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 # from django.http import Http404
-from rest_framework import generics, permissions  # , mixins, status
+from rest_framework import generics, permissions, renderers  # , mixins, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -174,3 +174,12 @@ def api_root(request, format=None):
             "snippets": reverse("snippet-list", request=request, format=format),
         }
     )
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
